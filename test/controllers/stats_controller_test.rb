527d243684a1840e324b30575623bbf7333d0b1f
lizkca <lienxiong@outlook.com>
2025-11-18 20:40:36 +0800

    fix(calendar): add return home link on calendar page\n\n- add '返回首页' button in header\n- test ensures link appears

--- test/controllers/stats_controller_test.rb
+++ test/controllers/stats_controller_test.rb
@@ -9,5 +9,6 @@ class StatsControllerTest < ActionDispatch::IntegrationTest
   test "calendar loads" do
     get "/calendar", headers: { "HTTP_COOKIE" => "visitor_id=testvid" }
     assert_response :success
+    assert_includes @response.body, "返回首页"
   end
 end
